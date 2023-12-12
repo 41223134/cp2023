44c133fcee1ce91bbe42b402b29e1f2bca9eb97d
41223134 <144767696+41223134@users.noreply.github.com>
2023-12-12 10:49:01 +0000

    設定gnuplot環境，放入w5執行步驟與結果

--- replit.nix
+++ replit.nix
@@ -1,15 +1,5 @@
 { pkgs }: {
   deps = [
-    pkgs.libev
-    pkgs.pandoc
-    pkgs.glibcLocales
-    pkgs.gitFull
+    pkgs.gnuplot
   ];
-  env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.libev
-      pkgs.glibcLocales
-      pkgs.gitFull
-    ];
-  };
 }
