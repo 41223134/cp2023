a7a05f5878558d7077277c613aab52dff1aae640
41223134 <144767696+41223134@users.noreply.github.com>
2023-12-18 09:40:52 +0000

    新增w6頁面、roc國旗半成品

--- replit.nix
+++ replit.nix
@@ -1,15 +1,7 @@
 { pkgs }: {
-  deps = [
-    pkgs.libev
-    pkgs.pandoc
-    pkgs.glibcLocales
-    pkgs.gitFull
-  ];
-  env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.libev
-      pkgs.glibcLocales
-      pkgs.gitFull
+    deps = [
+      pkgs.gnuplot
+      pkgs.ncurses.dev
+      pkgs.gd
     ];
-  };
 }
