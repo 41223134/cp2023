b1ed810d741832b56956e155ba99e735c0d57e48
41223134 <144767696+41223134@users.noreply.github.com>
2023-12-19 06:50:03 +0000

    新增w2

--- replit.nix
+++ replit.nix
@@ -1,11 +1,11 @@
 { pkgs }: {
     deps = [
-      pkgs.libev
-      pkgs.pandoc
-      pkgs.glibcLocales
-      pkgs.gitFull
-      pkgs.gnuplot
-      pkgs.ncurses.dev
-      pkgs.gd
+  pkgs.qtile
+  pkgs.sudo
+  pkgs.clang_12
+        pkgs.ccls
+        pkgs.gdb
+        pkgs.gnumake
+        pkgs.gnuplot
     ];
 }
