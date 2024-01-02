ed8df4f4e40a9837e6bc46bf2a174e846212a754
41223134 <144767696+41223134@users.noreply.github.com>
2024-01-02 08:49:19 +0000

    w12修改

--- replit.nix
+++ replit.nix
@@ -1,5 +1,11 @@
 { pkgs }: {
-  deps = [
-    pkgs.gnuplot
-  ];
+    deps = [
+        pkgs.sudo
+        pkgs.clang_12
+        pkgs.ccls
+        pkgs.gdb
+        pkgs.gnumake
+        pkgs.gnuplot
+        pkgs.gd
+    ];
 }
